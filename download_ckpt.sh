704ee54624e5385e979cb7e541f94f179ad33289
unknown <jchavan010@gmail.com>
2026-04-15 15:26:37 -0400

    fixed HF checkpoint download script

--- download_ckpt.sh
+++ download_ckpt.sh
@@ -1,6 +1,5 @@
-
 REPO_ID="Jchavan010/LaddFinal"
-LOCAL_DIR="/root/Grace/checkpoints"
+LOCAL_DIR="/root/Grace"
 
 echo "Installing huggingface_hub..."
 python -m pip install -U "huggingface_hub[cli]"
@@ -11,9 +10,8 @@ echo "Downloading ckpt into $LOCAL_DIR ..."
 
 hf download "$REPO_ID" \
   --repo-type model \
-  --include "checkpoints/checkpoint-20300/*" \
+  --include "checkpoints/checkpoint-20300/**" \
   --local-dir "$LOCAL_DIR"
 
 echo "Download complete. Listing files:"
-find "$LOCAL_DIR" -maxdepth 4 -type f | sort
-echo "Done."
+find "$LOCAL_DIR/checkpoints/checkpoint-20300" -type f | sort
